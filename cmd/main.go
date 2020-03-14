a55c41322a86a79a53edc7c7d08ce029da66c1fa
Tamara Jordan <tamara.jordan1+coding@hotmail.com>
2020-03-14 12:34:20 +0000

    Get response body from google

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+)
 
 func main() {
 	fmt.Println("Hello world!")
+
+	res, err := http.Get("http://google.com")
+
+	if err != nil {
+		fmt.Println("Error making a request to google :(")
+		os.Exit(1)
+	}
+
+	defer res.Body.Close()
+
+	bodyBytes, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		fmt.Println("Couldn't read the response body :(")
+	}
+
+	bodyString := string(bodyBytes)
+	fmt.Println(bodyString)
 }
